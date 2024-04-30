8c2eb622b4450f1d66c07f62a0d535c390365621
David Benner <119465113+davebenner14@users.noreply.github.com>
2024-04-30 10:59:20 -0400

    Update styling for cards and movement. also added background assets

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let position = 0; // Tracks the current front card index
 const cards = document.querySelectorAll(".card");
+const titles = document.querySelectorAll("h2");
 
 function rotateRight() {
   position = (position + 1) % 3;
@@ -21,5 +22,8 @@ function updateCards() {
     })`;
     card.style.opacity = offset === 0 ? 1 : 0.8;
     card.style.zIndex = offset === 0 ? 3 : 1;
+  });
+  titles.forEach((title, i) => {
+    title.style.opacity = i === position ? 1 : 0;
   });
 }
